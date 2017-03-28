369ff4bfe0c8456ca9a7660eadd59b9366db3ab4
NavyLegend <1593025619@qq.com>
2017-03-28 10:57:50 +0800

    自定义View

--- app/src/main/java/common/jlt/com/testdemo/customviewpager/CustomViewPager.java
+++ app/src/main/java/common/jlt/com/testdemo/customviewpager/CustomViewPager.java
@@ -2,6 +2,7 @@ package common.jlt.com.testdemo.customviewpager;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 import common.jlt.com.testdemo.R;
 
@@ -10,10 +11,19 @@ import common.jlt.com.testdemo.R;
  */
 public class CustomViewPager extends AppCompatActivity {
 
+    private CustomView container;
+    private int[] imageRes={R.mipmap.pic_1,R.mipmap.pic_2,R.mipmap.pic_6,R.mipmap.pic_4,R.mipmap.pic_5};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom_view_pager);
+        container = ((CustomView) findViewById(R.id.container));
+        for (int i = 0; i <imageRes.length ; i++) {
+            ImageView imageView=new ImageView(this);
+            imageView.setImageResource(imageRes[i]);
+            container.addView(imageView);
+        }
 
     }
 }
