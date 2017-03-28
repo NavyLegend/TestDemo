12513c6d16a833ad91f957ca57f35015700f2ca6
NavyLegend <1593025619@qq.com>
2017-03-28 19:44:22 +0800

    自定义View

--- app/src/main/java/common/jlt/com/testdemo/customviewpager/CustomVerticalActivity.java
+++ app/src/main/java/common/jlt/com/testdemo/customviewpager/CustomVerticalActivity.java
@@ -2,14 +2,23 @@ package common.jlt.com.testdemo.customviewpager;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 import common.jlt.com.testdemo.R;
 
 public class CustomVerticalActivity extends AppCompatActivity {
+    private CustomVerticalView container;
+    private int[] imageRes={R.color.material_deep_teal_20,R.color.text_color,R.color.red_btn_bg_color,R.color.material_blue_grey_80,R.color.material_deep_teal_50,R.color.error_stroke_color,R.color.warning_stroke_color};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom_vertical);
+        container = ((CustomVerticalView) findViewById(R.id.container));
+        for (int i = 0; i <imageRes.length ; i++) {
+            ImageView imageView=new ImageView(this);
+            imageView.setBackgroundResource(imageRes[i]);
+            container.addView(imageView);
+        }
     }
 }
--- app/src/main/java/common/jlt/com/testdemo/customviewpager/CustomVerticalView.java
+++ app/src/main/java/common/jlt/com/testdemo/customviewpager/CustomVerticalView.java
@@ -3,6 +3,7 @@ package common.jlt.com.testdemo.customviewpager;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
+import android.view.VelocityTracker;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
@@ -13,6 +14,14 @@ import android.widget.Scroller;
  */
 
 public class CustomVerticalView extends ViewGroup {
+    private int lastX;
+    private int lastY;
+    private int currentIndex = 0; //当前子元素
+    private int childWidth = 0;
+    private Scroller scroller;
+    private VelocityTracker tracker;    //增加速度检测,如果速度比较快的话,就算没有滑动超过一半的屏幕也可以
+    private int lastInterceptX=0;
+    private int lastInterceptY=0;
     private int mScreenHeight;
     private int mStartY;
     private int mEnd;
@@ -20,21 +29,66 @@ public class CustomVerticalView extends ViewGroup {
     private int mLastY;
     private int childCount;
 
+
     public CustomVerticalView(Context context) {
         super(context);
+        init();
     }
 
     public CustomVerticalView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        //初始化一些需要的属性
-        WindowManager wm = (WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE);
-        mScreenHeight = wm.getDefaultDisplay().getHeight();
-        mScroller = new Scroller(getContext());
+        init();
     }
 
     public CustomVerticalView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
     }
+    public void init(){
+        scroller=new Scroller(getContext());
+        tracker=VelocityTracker.obtain();
+        WindowManager wm = (WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE);
+        mScreenHeight = wm.getDefaultDisplay().getHeight();
+    }
+
+    /**
+     * 事件拦截
+     * @param
+     * @return
+     */
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent event) {
+        boolean intercept=false;
+        int x = (int) event.getX();
+        int y = (int) event.getY();
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                intercept=false;
+                //如果动画还没执行完，则打断
+                if (!scroller.isFinished())
+                    scroller.abortAnimation();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                int deltaX=x-lastInterceptX;
+                int deltaY=y-lastInterceptY;
+                if (Math.abs(deltaX)-Math.abs(deltaY)>0){
+                    intercept=true;
+                }else {
+                    intercept=true;
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                intercept=false;
+                break;
+
+        }
+        lastX=x;
+        lastY=y;
+        lastInterceptX=x;
+        lastInterceptY=y;
+        return intercept;
+    }
+
 
     //在onMeasure中测量子view
     @Override
@@ -47,7 +101,8 @@ public class CustomVerticalView extends ViewGroup {
         }
     }
 
-    //确定子View的位置
+
+    //确定子View的位子
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
         childCount = getChildCount();
@@ -59,83 +114,73 @@ public class CustomVerticalView extends ViewGroup {
         for (int i = 0; i < childCount; i++) {
             View childView = getChildAt(i);
             if (childView.getVisibility() != View.GONE) {
-                childView.layout(l, mScreenHeight * i, r, mScreenHeight * (i + 1));
+                childView.layout(l, i * mScreenHeight, r, (i + 1) * mScreenHeight);
             }
         }
+
     }
 
-    //增加我们需要的触摸响应事件
+
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        //在这个触摸事件中，需要判断两个距离，一个是手指移动的距离一个是view滚动的距离
-        //这是随着手指的移动会发送改变的量
-        int y = (int) event.getY();
-        switch (event.getAction()) {
+        tracker.addMovement(event);
+        int x= (int) event.getX();
+        int y= (int) event.getY();
+        switch (event.getAction()){
             case MotionEvent.ACTION_DOWN:
-                mLastY = y;
-                mStartY = getScrollY();
+                if (!scroller.isFinished())
+                    scroller.abortAnimation();
                 break;
             case MotionEvent.ACTION_MOVE:
-                //当我们再次触碰屏幕时，如果之前的滚动动画还没有停止，我们也让他立即停止
-                if (!mScroller.isFinished()){
-                    mScroller.abortAnimation();
-                }
-                int dY=mLastY-y;
-                //滚动触碰到上边缘时给个下拉反弹的效果
-                if (getScrollY()<0){
-                    dY/=3;
-                }
-                //判断滚动的
-                if (getScrollY()>mScreenHeight*(getChildCount()-1)){
-                    dY=0;
-                }
-                //让我们的view滚动相应的dy距离
-                scrollBy(0,dY);
-                mLastY=y;
+                //跟随手指滑动
+//                int deltaX=x-lastX;
+                int deltaY=y-lastY;
+
+                scrollBy(0,-deltaY);
                 break;
+            //释放手指以后开始自动滑动到目标位置
             case MotionEvent.ACTION_UP:
-                mEnd=getScrollY();
-                int dScrollY=mEnd-mStartY;
-                //向上滚动
-                if (dScrollY>0){
-                    if (getScrollY()<0){
-                        mScroller.startScroll(0,getScrollY(),0,-dScrollY);
-                    }else {
-                        if (dScrollY<mScreenHeight/3)
-                            mScroller.startScroll(0,getScrollY(),0,-dScrollY);
+                //相对于当前View滑动的距离,正为向左,负为向右
+                int distance=getScrollY()-currentIndex*mScreenHeight;
+                //必须滑动的距离要大于1/2个宽度,否则不会切换到其他页面
+                if (Math.abs(distance)>mScreenHeight/2){
+                    if (distance>0)
+                        currentIndex++;
+                    else
+                        currentIndex--;
+                }else {
+                    tracker.computeCurrentVelocity(1000);
+                    float yV=tracker.getYVelocity();
+                    if (Math.abs(yV)>50){
+                        if (yV>0)
+                            currentIndex--;
                         else
-                            mScroller.startScroll(0,getScrollY(),0,mScreenHeight-dScrollY);
+                            currentIndex++;
                     }
-
-                }else
-                //向下滚动
-                {
-                    if (getScrollY()>mScreenHeight*(getChildCount()-1)){
-                        mScroller.startScroll(0,getScrollY(),0,-dScrollY);
-                    }else {
-                        if (-dScrollY<mScreenHeight/3)
-                            mScroller.startScroll(0,getScrollY(),0,-dScrollY);
-                        else
-                            mScroller.startScroll(0,getScrollY(),0,-mScreenHeight-dScrollY);
-                    }
-
                 }
+                currentIndex=currentIndex<0?0:currentIndex>getChildCount()-1?getChildCount()-1:currentIndex;
+                smoothScrollTo(0,currentIndex*mScreenHeight);
+                tracker.clear();
                 break;
             default:
                 break;
         }
-        //重绘界面
-        postInvalidate();
+        lastX=x;
+        lastY=y;
+
         return true;
     }
-    //滑屏控制
 
+    private void smoothScrollTo(int destX, int destY) {
+        scroller.startScroll(getScrollX(),getScrollY(),destX-getScrollX(),destY-getScrollY(),1000);
+        invalidate();
+    }
 
     @Override
     public void computeScroll() {
         super.computeScroll();
-        if (mScroller.computeScrollOffset()){
-            scrollTo(0,mScroller.getCurrY());
+        if (scroller.computeScrollOffset()){
+            scrollTo(scroller.getCurrX(),scroller.getCurrY());
             postInvalidate();
         }
     }
